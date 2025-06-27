17754af68fc2e025731ff012ff2b8a1d183b633e
malaikaramzan52 <malaikaramzan52@gmail.com>
2025-06-27 00:14:17 -0700

    Array Sort

---
+++ Array Sort.js
@@ -0,0 +1,19 @@
+//sort()
+
+let fruits=['Mango','Orange','Banana','Grapes','Kiwi','strawberry','apple'];
+let fruit=fruits.sort();
+let f=fruits.reverse();
+
+//toSorted()
+//The difference between toSorted() and sort() is that the first method creates a new array,
+// keeping the original array unchanged, while the last method alters the original array.
+
+let months=['jan','feb','mar','june','april','sep'];
+let sort=months.toSorted();
+
+//toReversed()
+
+//The difference between toReversed() and reverse() is that the first method creates a new array,
+//  keeping the original array unchanged, while the last method alters the original array.
+
+let rev=months.toReversed();
