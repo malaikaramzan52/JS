56a4b0bcad404bfbf567f5b6fc82817bc05f741a
malaikaramzan52 <malaikaramzan52@gmail.com>
2025-07-08 03:44:54 -0700

    Functions

---
+++ Functions.js
@@ -0,0 +1,26 @@
+//Functions
+// A JavaScript function is a block of code designed to perform a particular task.
+// A JavaScript function is executed when "something" invokes it (calls it).
+
+//Functions
+//Functions definition-----> do some work
+//Functions call/invoke--->call to do work
+
+//to remove redundancy
+
+
+function myFunction(){
+    console.log("Welcome to my WORLD!!!");
+}
+myFunction();
+
+function para(msg ,n){
+    console.log(msg);
+}
+//varaible does not have value will be undefined
+
+para("I love JS",100);
+
+//value pass to function during function definition---->parameters
+//value pass to function during function call----->arguments
+
---
+++ Value.js
@@ -0,0 +1,10 @@
+function values(arr) {
+  return arr;
+}
+
+
+let fruits=['apple','mango','orange','banana'];
+let text = "";
+for(let x of values(fruits)){
+    text += x + "<br>";
+}
