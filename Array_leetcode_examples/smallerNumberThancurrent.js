4865987a17f2d02ec25ed9448eb60f54f62d93ed
malaikaramzan52 <malaikaramzan52@gmail.com>
2025-07-13 23:02:31 -0700

    smallerNumbersThanCurrent

---
+++ Array_leetcode_examples/smallerNumberThancurrent.js
@@ -0,0 +1,21 @@
+let nums = prompt("Enter the numbers separated by commas:");
+nums = nums.split(",").map(Number);
+
+function smallerNumbersThanCurrent(nums) {
+    let result = new Array(nums.length); // Result array of same size);
+
+    for (let i = 0; i < nums.length; i++) {
+        let count = 0;
+        for (let j = 0; j < nums.length; j++) {
+            if (nums[i] > nums[j]) {
+                count++;
+            }
+        }
+        result[i] = count;
+    }
+
+
+    return result;
+}
+const output = smallerNumbersThanCurrent(nums);
+console.log(output); 
