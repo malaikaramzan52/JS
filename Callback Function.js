396b28eb231a4242bb92651115eab9d9e67d0bea
malaikaramzan52 <malaikaramzan52@gmail.com>
2025-07-21 00:45:34 -0700

    callback Function

---
+++ Callback Function.js
@@ -0,0 +1,75 @@
+//A callback function is passed as an argument to another function
+//  and is executed after the completion of that function.
+
+
+function num(n,callback){
+    return callback(n);
+
+}
+const double = (n) => n*2;
+console.log(num(5,double));
+
+
+//practice question_1
+
+function greet(myname,callback){
+    console.log("Hello " + myname);
+    callback();
+}
+function saygoodbye(){
+    console.log("God Bye!");
+}
+greet("Malaika",saygoodbye);
+
+
+//practice question_2
+
+
+function calculate(num1,num2,callback){
+    const result=num1+num2;
+    callback(result);
+}
+function displayresult(result){
+    console.log("The result is "+ result);
+}
+calculate(10,5,displayresult);
+
+
+//practice question_3
+ function processAnswer(question,callback){
+    console.log("Hello "+ question);
+    callback();
+ }
+ function answer(){
+    console.log("I am fine");
+ }
+
+ processAnswer("How are you ?",answer);
+
+
+ //practice question_4
+ const fruits=["apple","banana","cherry"];
+
+fruits.forEach(function(fruit,index){
+    console.log(`Fruit ${index+1}: ${fruit}`);
+});
+
+//Practice question_5
+function loadProfile(callback) {
+    console.log("Loading profile..."); // Removed invalid profile access
+
+    setTimeout(function () {
+        const profile = {
+            Fullname: "Ali",
+            Age: 23
+        };
+        callback(profile); // Pass the profile to the callback
+    }, 3000);//after 3 sec
+}
+
+// Call loadProfile and pass an anonymous callback function
+loadProfile(function (profile) {
+    console.log("Profile Loaded:", profile);
+});
+
+
--- Self Executing Funtion.js
+++ Self Executing Funtion.js
@@ -2,9 +2,11 @@
 //This function is initialized inside a set of round brackets and the parameters can be passed through the round brackets at the end.
 
 (function(){
-    date = new Date(),toString();
+   const  date = new Date();
+   toString();
     console.log(date);
 })();
+// console.log(date);
 
 //consists of two expression --->A new Date object is created and assigned to a variable named date.
 // Important: No var, let, or const is used, so date becomes a global variable (bad practice). 
