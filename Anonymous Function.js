c40f028c7d95ed3511584c74194711eff6db2042
malaikaramzan52 <malaikaramzan52@gmail.com>
2025-07-21 23:19:13 -0700

    Anonymous function

---
+++ Anonymous Function.js
@@ -0,0 +1,10 @@
+// Anonymous Function
+//Anonymous functions are functions without a name. 
+// They are often used as arguments to other functions.
+
+
+setTimeout(function(){
+    console.log("Anonymous function executed!");
+},3000);
+
+//time in milli seconds
