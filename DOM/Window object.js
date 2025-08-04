48801a9ef36a047d9dd9dad9a22cb758a61cd4c3
malaikaramzan52 <malaikaramzan52@gmail.com>
2025-08-04 04:00:58 -0700

    window object

---
+++ DOM/Window object.js
@@ -0,0 +1,6 @@
+//THe winow Object represents an open window in a browser. It is browser's object (not Javascript) & is automatically
+//created by browser.
+// It is a global object with lots of properties & methods.
+console.log("Hello");
+window.console.log("Hello2");
+window.alert("Hi");
