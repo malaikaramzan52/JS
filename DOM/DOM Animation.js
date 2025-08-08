052c73045af27dad17a116e07ae15f58c0d3e8af
malaikaramzan52 <malaikaramzan52@gmail.com>
2025-08-08 04:00:39 -0700

    Animations

---
+++ DOM/DOM Animation.js
@@ -0,0 +1,12 @@
+//Animation ka matlab hota hai cheez ko time ke sath change karna — jaise size, color, position, shape.
+let box=document.getElementById("box");
+box.onclick=function(){
+    box.style.backgroundColor="red";
+}
+
+//position move animation
+
+let ball=document.getElementById("ball");
+ball.onclick=function(){
+    ball.style.left ="600px";
+}
