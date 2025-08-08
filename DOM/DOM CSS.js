8eeed5b8a3fcd934ff80d7a9c497190bf41dcaaa
malaikaramzan52 <malaikaramzan52@gmail.com>
2025-08-08 02:58:16 -0700

    DOM CSS

---
+++ DOM/DOM CSS.js
@@ -0,0 +1,7 @@
+//CHange color of paragraph
+let para=document.getElementById("para");
+let btn=document.getElementById("btn");
+
+function changebtn(){
+    document.getElementById("para").style.color="red";
+}
