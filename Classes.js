2f1bc2895d36169340513115b4c74367853f620f
malaikaramzan52 <malaikaramzan52@gmail.com>
2025-08-13 01:22:32 -0700

    Classes

---
+++ Classes.js
@@ -0,0 +1,151 @@
+//Prototype
+
+//A javascript object is an entity having state and behavior (properties and methods).
+const student = {
+    name: "John",
+    age: 21,
+    printAge: () => {
+        console.log("Age is:", student.age);
+    },
+};
+//prototype--->is an object inside an object
+//prototype---->having own properties and methods 
+
+
+//objects in js inherits some properties snd methods from prototype
+
+const employee = {
+    calTax() {
+        console.log("Tax is calculated :10%");
+    },
+};
+const karanArjun1 = {
+    salary: 50000,
+};
+const karanArjun2 = {
+    salary: 50000,
+};
+const karanArjun3 = {
+    salary: 50000,
+};
+const karanArjun4 = {
+    salary: 50000,
+};
+//prototype
+karanArjun1.__proto__ = employee; //karanArjun is inheriting properties and methods from employee
+karanArjun2.__proto__ = employee;
+karanArjun3.__proto__ = employee;
+karanArjun4.__proto__ = employee;
+//type of prototype is refrence to an object or null.
+const karanArjun5 = {
+    salary: 60000,
+    calTax() {
+        console.log("Tax is calculated :12%");
+    },
+};
+karanArjun5.__proto__ = employee; //karanArjun5 is inheriting properties and methods from employee
+//If the object & prototype have same method, then the object method will be called.
+
+//typeof __proto__ ="object"
+
+
+
+//////######################################### CLASSES #####################################//////
+//Class is a program-code template for creating objects.
+//Those objects will have some state (variables) and behavior (functions) inside it.
+//JavaScript Classes are templates for JavaScript Objects.
+
+
+class ToyotaCar {
+    constructor(brand,mileage){
+        console.log("creating new object");
+        this.brandName=brand;
+        this.mileage=mileage;
+    }
+    start() {
+        console.log("Car started");
+    }
+    stop() {
+        console.log("Car stopped");
+    }
+    // setBrand(brand){
+    //     this.brandName=brand;
+    // }
+}
+let fortuner = new ToyotaCar("fortuner",10); //creating an object of class ToyotaCar
+// fortuner.setBrand("fortuner");
+let Lexus = new ToyotaCar("Lexus");
+// Lexus.setBrand("Lexus");
+
+
+////########################################## CONSTRUCTOR ###############################################
+
+
+//constructor() is reserved word.
+//constructor() method is a special method for creating and initializing an object created with a class.
+//automatically invoked by new object is created
+
+//initilizes object
+//At the time of initiliziation to set the value ....constructor is used.
+
+class car{
+    constructor(name,year){
+        this.name=name;
+        this.year=year;
+        console.log("TThe constructor is called");
+    }
+    yearsOld(){
+        const date=new Date();
+        return date.getFullYear()-this.year;
+    }
+}
+const myCar = new car("ford", 2015);
+console.log(myCar.yearsOld());
+
+
+//In "strict mode" you will get an error if you use a variable without declaring it
+"use strict";
+class X{
+    constructor(a , b){
+           this.sum=a+b;
+
+    }
+}
+const myX= new X(5,10);
+console.log(myX.sum);
+
+//Example_01
+class Book{
+    constructor(title,author,year){
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+    getSummary(){
+        return `${this.title} was written by ${this.author} in ${this.year}.`;
+    }
+}
+
+const Book1=new Book("Book One","John Doe",2020);
+const Book2=new Book("Book Two","Lessy Johnson",2021);
+console.log(Book1.getSummary());
+console.log(Book2.getSummary());
+
+//Example_02
+class Rectangle{
+    constructor(width,height){
+        this.width = width;
+        this.height = height;
+
+    }
+    getArea(){
+        return this.width*this.height;
+    }
+    getPerimeter(){
+        return 2*(this.width + this.height);
+    }
+}
+const rect1=new Rectangle(5,10);
+console.log("Area of rectangle:", rect1.getArea());
+console.log("Perimeter of rectangle:", rect1.getPerimeter());
+
---
+++ TypeCasting.js
@@ -0,0 +1,23 @@
+console.log("Allah");
+//converting Strings to number.
+// Number("3.14");
+console.log(Number("3.14")); // 3.14
+console.log(Math.PI);
+console.log(Number(" "));
+console.log(Number(""));
+console.log(Number("99 88"));
+console.log(Number("John"));
+
+console.log(parseFloat("3.14"));
+console.log(parseInt("3.14"));
+
+//Number to String
+let x=3.14;
+console.log(x.toString());
+console.log(String(Math.PI));
+console.log((true).toString());
+console.log(String(false));
+console.log(String(null));
+console.log(String(undefined));
+console.log((123).toString());
+console.log((100 + 23).toString());
